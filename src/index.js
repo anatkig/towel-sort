15b336b7364028ba118fca36824d9afd13b74222
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-10-01 11:08:39 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@
 module.exports = function towelSort (matrix) {
     
     if(!matrix) return [];
-  return matrix.reduce((sum,a)=>sum.concat(a.sort((c,b)=>{if(matrix.indexOf(a)%2!==0)return c-b; else return b-c})));
+  return matrix.reduce((sum,a)=>sum.concat(a.sort((c,b)=>{if(matrix.indexOf(a)%2===0)return c-b; else return b-c})));
 }
