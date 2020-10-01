6a10bf4b43e034bac76bb0d89f7f75d5552c2d23
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-10-01 09:55:09 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return matrix.reduce((sum,a)=>sum.concat(a)).sort((a,b)=>a-b);
+  return matrix.reduce((sum,a)=>sum.concat(a.sort((a,b)=>{if(matrix.indexof(a)%2!==0)return a-b; else return b-a})));
 }
